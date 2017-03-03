f2fd377b061da69e737a7a77f77b81aa130562bc
Ikillnukes <ikillnukes1@gmail.com>
2017-03-04 00:29:06 +0100

    Terminado el ejercicio 5 de forma avanzada

--- Parte 1/5.sh
+++ Parte 1/5.sh
@@ -16,27 +16,30 @@ read -p "Introduce el nombre de usuario que desea comprobar si existe: " user #O
 #El proposito de esta parte es eliminar los resultados creados por el sistema, y mostrar los resultados a partir de la
 #linea donde estan los usuarios creados por nosotros
 
-while read u;
-do
-	if [[ $u == *"$user"* ]];
-	then
-		primuser=`echo $u | cut -d ':' -f1`
-		break
-	fi
-done < /etc/passwd
+#NOTA DESPUÉS DE LA CREACIÓN: Realmente esto es inutil, porque si tu creas un usuario con un nombre que ya contiene
+#el archivo /etc/passwd, lo hayas creado tu o no, no te va a dejar.
 
-echo $primuser
+while read u; #El primer proposito de este bucle es obtener el primer usuario de la lista del passwd, para saber por donde cortar con el tail luego
+do #Creamos un bucle while que nos liste todas lineas del archivo /etc/passwd
+	for us in `ls /home/`;
+	do #Ahora listamos todas las carpetas del /home/ (para obtener el nombre de usuario ya existente)
+		if [[ $u == *"$us"* ]];
+		then #Si coincide el nombre de la carpeta con el nombre del usuario del while significa que...
+			primuser=$us #... almacenaremos el nombre de usuario y...
+			break 2 #... y romperemos los 2 bucles, ya que hemos obtenido el primer usuario de la lista
+		fi
+	done
+done < /etc/passwd #Injeccion del archivo el cual queremos leer
 
-exit 1
+numlinea=`grep -nr $primuser /etc/passwd | cut -d ':' -f1` #Obtenemos la linea donde está el nombre del usuario gracias a -nr que nos devuelve algo tal que "25:...", entonces cortamos hasta los puntos suspensivos para obtener el 25
+totallineas=`wc -l /etc/passwd | cut -f1 -d ' '` #Obtenemos el total de lineas de /etc/passwd, usando cut para obtener lo que nos interesa únicamente
+finlinea=$(($totallineas-$numlinea+1)) #Obtenemos las lineas que hay desde donde está el primer usuario que creamos hasta el final, en nuestro caso, como creamos 3 usuarios nos va a mostrar el numero 3
+contlistado=`tail -$finlinea /etc/passwd` #Y recopilamos esta información, que son correspondientes a mi caso a las ultimas 3 lineas
 
-numlinea=`grep -nr $primuser /etc/passwd | cut -d ':' -f1`
-totallineas=`wc -l /etc/passwd`
-finlinea=`let totallinea-numlinea+1`
-contlistado=`tail -$finlinea /etc/passwd`
-
-if [ `$contlistado | grep $user | wc -l` > 1 ];
-then
+if [[ `echo $contlistado | grep -o $user` != "" ]];
+then #Ya con todo esto, lo unico que tenemos que hacer es averiguar si existe o no el usuario que especificamos al principio o no, para ello usamos -o, que lo que hace es devolvernos una cadena con texto si se encuentra algo o vacia
+#si no lo hace...
 	echo "El usuario '$user' existe."
 else
-	echo "El usuario '$user' no existe." #Si el bucle acaba quiere decir que no ha encontrado nada, por tanto, ya es hora de notificarlo
+	echo "El usuario '$user' no existe."
 fi

605ae2737ae0dabe5a8d0b40d31463556bae76a8
Ikillnukes <ikillnukes1@gmail.com>
2017-03-06 16:55:22 +0100

    Acabo ejercicio 1.sh

--- Proyecto final/1.sh
+++ Proyecto final/1.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 OIFS=$IFS
-IFS="ñ"
+IFS="|"
 
-lines=`sort /etc/passwd | tr "\n" "ñ"`
+lines=`sort /etc/passwd | tr "\n" "|"`
+linesarr=($lines)
 
-for line in $lines[@];
+for ((i = 0; i<${#linesarr[@]}; ++i))
 do
+	line=${linesarr[$i]}
         #test=`echo $line | cut -d: -f6`
         #if [[ $line == *":"* && $test != "" ]];
         #then
                 echo "Usuario: $(echo $line | cut -d: -f1)"
-                echo "Intrprete: $(echo $line | cut -d: -f6)"
-                echo "Directorio: $(echo $line | cut -d: -f7)"
+		echo "Contraseña: $(echo $line | cut -d: -f2)"
+		echo "Nombre: $(echo $line | cut -d: -f5)"
+                echo "Directorio: $(echo $line | cut -d: -f6)"
+                echo "Intérprete: $(echo $line | cut -d: -f7)"
                 echo ""
         #fi
 done
